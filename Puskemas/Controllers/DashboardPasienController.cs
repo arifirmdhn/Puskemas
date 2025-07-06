b8967698948794a0da120c95a49abfad35fbec99
mohd22si <mohd22si@mahasiswa.pcr.ac.id>
2025-07-07 03:59:35 +0700

    Progress Ilham

---
+++ Puskemas/Controllers/DashboardPasienController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize(Roles = "Pasien")]
+public class PasienController : Controller
+{
+    public IActionResult DashboardPasien()
+    {
+        ViewBag.Reservasi = new List<string> { "Reservasi A", "Reservasi B" };
+        ViewBag.RekamMedis = new List<string> { "Data Medis 1", "Data Medis 2" };
+        ViewBag.JadwalDokter = new List<string> { "Senin - 08.00", "Selasa - 10.00" };
+        ViewBag.InformasiReservasi = new List<string> { "Detail 1", "Detail 2" };
+        return View();
+    }
+}
